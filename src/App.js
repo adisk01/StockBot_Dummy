fcb5dd1006fb4ea237f25872fec57f0a3fc2db15
adisk01 <adiscrazy01@gmail.com>
2024-02-26 02:59:16 +0530

    fron-end

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./normal.css";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import LoginForm from "./components/login/LoginForm";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="auth/login" />;
+  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
